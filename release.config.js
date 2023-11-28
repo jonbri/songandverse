3cb3a8be6f5a9919cb55a50d55ba8f2452bc3c07
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-11-28 16:21:03 -0500

    fix: manual changelog

--- release.config.js
+++ release.config.js
@@ -2,13 +2,6 @@ module.exports = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        releaseCount: 0,
-      },
-    ],
-    "@semantic-release/changelog",
     "@semantic-release/npm",
     require("./semantic-release-plugin"),
   ],
--- semantic-release-plugin.js
+++ semantic-release-plugin.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 
-// require('conventional-changelog')({
-//   preset: 'angular',
-//   releaseCount: 0
-// }).pipe(process.stdout);
-
 async function prepare(config, context) {
-  const changeLogPath = "CHANGELOG.md";
-  console.log("changeLogPath", changeLogPath);
-  if (!fs.existsSync(changeLogPath)) {
-    throw new Error(`Cannot find ${changeLogPath}`);
-  }
-  const content = fs.readFileSync(changeLogPath, "utf8");
-  console.log("changelog dump");
-  console.log(content);
+  // const changeLogPath = "CHANGELOG.md";
+  // console.log("changeLogPath", changeLogPath);
+  // if (!fs.existsSync(changeLogPath)) {
+  //   throw new Error(`Cannot find ${changeLogPath}`);
+  // }
+  // const content = fs.readFileSync(changeLogPath, "utf8");
+  // console.log("changelog dump");
+  // console.log(content);
+
+  require("conventional-changelog")({
+    preset: "angular",
+    releaseCount: 0,
+  }).pipe(process.stdout);
 }
 
 module.exports = { prepare };
