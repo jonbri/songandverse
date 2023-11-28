a71252f91fa57927df165d576be886ae0f37ff07
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-11-28 15:51:53 -0500

    fix: correct config

--- release.config.js
+++ release.config.js
@@ -2,14 +2,14 @@ module.exports = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
     [
-      require("./semantic-release-plugin"),
+      "@semantic-release/release-notes-generator",
       {
         linkCompare: false,
-      }
-    ]
-  ]
+      },
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    require("./semantic-release-plugin"),
+  ],
 };
--- semantic-release-plugin.js
+++ semantic-release-plugin.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
+const fs = require("fs");
 
 async function prepare(config, context) {
-  const changeLogPath = 'CHANGELOG.md';
-  console.log('changeLogPath', changeLogPath);
+  const changeLogPath = "CHANGELOG.md";
+  console.log("changeLogPath", changeLogPath);
   if (!fs.existsSync(changeLogPath)) {
     throw new Error(`Cannot find ${changeLogPath}`);
   }
-  const content = fs.readFileSync(changeLogPath, 'utf8');
-  console.log('content', content);
+  const content = fs.readFileSync(changeLogPath, "utf8");
+  console.log("changelog dump");
+  console.log(content);
 }
 
 module.exports = { prepare };
-
