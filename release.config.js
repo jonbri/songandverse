4b5a587d84f28824b4b037998891c42144b2e85d
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-11-28 13:50:41 -0500

    fix: do not track changelog

--- release.config.js
+++ release.config.js
@@ -5,13 +5,6 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        "assets": ["CHANGELOG.md"]
-      }
-    ],
-    require('./semantic-release-plugin'),
-    "@semantic-release/github"
+    require('./semantic-release-plugin')
   ]
 };
