3068f07b0f7c4ff3bc23f09d596a7267f5fcc571
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-01 14:54:30 -0500

    fix: ci

--- release.config.js
+++ release.config.js
@@ -1,12 +1,26 @@
 module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/npm",
       {
         pkgRoot: "./dist",
       },
     ],
-    require("./src/plugin/semantic-release-plugin"),
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "./CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: "./CHANGELOG.md",
+      },
+    ],
+
+    // require("./src/plugin/semantic-release-plugin"),
   ],
 };
